74f9c747982301163998a5e01bb3da274aea817f
Anay Wadhera <awadhera@berkeley.edu>
2020-01-14 18:07:21 -0800

    themepicker: icon packs
    Change-Id: I4b82bae7305a4fe69ac4a1c916e45c09ec40cccd

---
+++ src/com/android/customization/picker/iconpack/IconPackFragment.java
@@ -0,0 +1,11 @@
+import com.android.wallpaper.picker.ToolbarFragment;
+
+public class IconPackFragment extends ToolBarFragment {
+
+     /**
+     * Interface to be implemented by an Activity hosting a {@link ThemeFragment}
+     */
+    public interface ThemeFragmentHost {
+        ThemeManager getThemeManager();
+    }
+}
